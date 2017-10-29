85e606372c447bb57d67e8f3d04ea548fc8bf2fe
RSmetanyuk <rsmetanyuk@gmail.com>
2017-10-29 10:48:53 +0200

    Added navbar

---
+++ src/client/app/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export class NavBar extends React.Component {
+  render() {
+    const pages = ['Championships', 'Teams', 'Matches', 'About'];
+    const navLinks = pages.map(page => {
+      return (
+        <li key={page}>
+          <a href={'/' + page}>
+            {page}
+          </a>
+        </li>
+      )
+    });
+
+    return (
+      <div className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <a href="#"><img src="img/football.png" alt="Soccer ball"></img></a>
+          <ul className="nav navbar-nav">            
+            <nav>{navLinks}</nav>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {NavBar} from './NavBar';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React project!</p>;
+    return (
+      <div>
+        <NavBar />
+        <div className="container" id="main-content">
+        </div>
+      </div>
+    );
   }
 }
 
