e576631f4e4f8c7b13dfdbff486eec1d9b3af602
RSmetanyuk <rsmetanyuk@gmail.com>
2017-11-01 22:12:16 +0200

    Code improvements

--- src/Championships.jsx
+++ src/Championships.jsx
@@ -11,27 +11,25 @@ export class Championships extends React.Component {
         .then((result) => {
           return result.json()
         }).then((answer) => {
-            var res = answer.result.map(function(championship) {
+            const res = answer.result.map(championship => {
                 championship.image = "https://footballbet.com.ua/table/embl/" + championship.image;
                 return championship
-             })
+            })
           this.setState({championships: res})
         })
     }
     
     render() {
-        const tableRows = this.state.championships.map(function(championship) {
-            return (
+        const tableRows = this.state.championships.map(championship => (
                 <tr key={championship.id_championship}>
                     <td>
-                        <Link to={'/championship/' + championship.title}>
+                        <Link to={'/championships/' + championship.title}>
                             <img src={championship.image} height="35" width="35" />
                             {championship.name}
                         </Link>
                     </td>
                 </tr>
-            );
-        });
+        ));
 
         return(
             <table className="table-championships">
