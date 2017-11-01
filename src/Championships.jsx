6eabe8af8d4a21236e761138330a08e527a00da7
RSmetanyuk <rsmetanyuk@gmail.com>
2017-11-01 18:33:22 +0200

    Added fetch championships api

--- src/Championships.jsx
+++ src/Championships.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from 'react'
 
 export class Championships extends React.Component {
+	constructor() {
+        super();
+        this.state={championships:[]};
+    }
+    componentDidMount(){
+        fetch('https://footballbet.com.ua/api/championships/')
+        .then((result) => {
+          return result.json();
+        }).then((answer) => {
+          this.setState({championships: answer.result});
+        })
+    }
+    
     render() {
-        return (
-           <div>
-              <h1>Championships...</h1>
-           </div>
-        )
-     }
+        return(
+          <ul>
+            {
+                this.state.championships.length ? 
+                this.state.championships.map(championship=><li key={championship.id_championship}>{championship.name}</li>) : 
+                <li>Loading...</li>
+            }
+        </ul>
+     )
+    }
 }
