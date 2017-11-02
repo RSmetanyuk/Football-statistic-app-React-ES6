5876a33d75b4c995821d86e17d4cb8146aeacce6
RSmetanyuk <rsmetanyuk@gmail.com>
2017-11-02 21:08:11 +0200

    Fixed default route

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import { NavBar } from './NavBar.jsx';
 import { Championships } from './Championships.jsx';
+import { ChampionshipsAll } from './ChampionshipsAll.jsx';
 import { Teams } from './Teams.jsx';
 import { Matches } from './Matches.jsx';
 import { About } from './About.jsx';
@@ -13,7 +14,7 @@ const App = () => (
     <NavBar />
     <div className="container" id="main-content">
       <Switch>
-        <Route exact path='/' component={Championships}/>
+        <Route exact path='/' component={ChampionshipsAll}/>
         <Route path='/championships' component={Championships}/>
         <Route path='/teams' component={Teams}/>
         <Route path='/matches' component={Matches}/>
