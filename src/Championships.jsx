1c94144a494110cf4cbc3b629826759c2a6df7d0
RSmetanyuk <rsmetanyuk@gmail.com>
2017-11-09 18:54:34 +0200

    Added pagination

--- src/Championships.jsx
+++ src/Championships.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { GetApi } from './GetApi.jsx'
 import { Switch, Route } from 'react-router-dom'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 
 let loaded = [];
 
@@ -20,8 +21,12 @@ export class Championships extends React.Component {
             GetApi('championships').then((arr) => {
                 const modArr = arr.map(item => {
                     item.image = "https://footballbet.com.ua/table/embl/" + item.image;
+                    item.link = <Link to={'/championships/' + item.title}>
+                        <img src={item.image} height="35" width="35" />
+                        {item.name}
+                        </Link>
                     return item
-                });
+                });                
                 this.setState({
                     championships: modArr,
                     loading: false
@@ -35,41 +40,39 @@ export class Championships extends React.Component {
     }
         
     render() {
+        const {championships} = this.state;
         const championshipsAll = () => {
             if (this.state.loading) {
                 return <h2>Loading...</h2>;
             }
+            const colFormatter = (cell, row) => {
+                return (
+                    <Link to={'/championships/' + row.title}>
+                        <img src={row.image} height="35" width="35" />
+                        {cell}
+                    </Link>
+                )
+            }
             return (
-                <table className="table-championships">
-                <tbody>
-                    {
-                        this.state.championships.map(championship => (
-                            <tr key={championship.id_championship}>
-                                <td>
-                                    <Link to={'/championships/' + championship.title}>
-                                        <img src={championship.image} height="35" width="35" />
-                                        {championship.name}
-                                    </Link>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+                <div>
+                    <BootstrapTable data={ championships } pagination>
+                        <TableHeaderColumn dataField='name' dataFormat={ colFormatter } isKey={ true }>Championships</TableHeaderColumn>
+                    </BootstrapTable>
+                </div>
             )
         }
         const championship = (props) => {
             const champ = (item) => item.title === props.match.params.name;
-            const index = this.state.championships.findIndex(champ);
+            const index = championships.findIndex(champ);
             if (this.state.loading) {
                 return <h2>Loading...</h2>;
             }
             return (
                 <div className="container left">
-                    <img src={this.state.championships[index].image} />
-                    <p>ID чемпіонату: {this.state.championships[index].id_championship}</p>
-                    <p>Чемпіонат: {this.state.championships[index].name}</p>
-                    <p>Назва чемпіонату: {this.state.championships[index].sename}</p>
+                    <img src={championships[index].image} />
+                    <p>ID чемпіонату: {championships[index].id_championship}</p>
+                    <p>Чемпіонат: {championships[index].name}</p>
+                    <p>Назва чемпіонату: {championships[index].sename}</p>
                 </div>              
             )
         }
