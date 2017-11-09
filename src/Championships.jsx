d1ee31b9dcf0afe7345249766c4e9dad1dde478d
RSmetanyuk <rsmetanyuk@gmail.com>
2017-11-09 20:13:04 +0200

    Centered Championships table

--- src/Championships.jsx
+++ src/Championships.jsx
@@ -55,7 +55,7 @@ export class Championships extends React.Component {
             }
             return (
                 <div>
-                    <BootstrapTable data={ championships } pagination>
+                    <BootstrapTable data={ championships } pagination className='table-championships'>
                         <TableHeaderColumn dataField='name' dataFormat={ colFormatter } isKey={ true }>Championships</TableHeaderColumn>
                     </BootstrapTable>
                 </div>
