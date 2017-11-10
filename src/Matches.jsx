6dba9a4921a0f22e631752d65072a5b64c74306c
RSmetanyuk <rsmetanyuk@gmail.com>
2017-11-10 16:27:09 +0200

    Added match page data

--- src/Matches.jsx
+++ src/Matches.jsx
@@ -21,19 +21,19 @@ export class Matches extends React.Component {
             GetApi('matches').then((arr) => {             
                 const modArr = arr.map(item => {
                     item.teamPair = item.firstTeam + ' - ' + item.secondTeam; 
-                    return item
+                    item.score = item.firstTeamGoal + ' - ' + item.secondTeamGoal;
+                    return item 
                 });                
                 this.setState({
                     matches: modArr,
                     loading: false
                 });
-                console.log(this.state.matches)
             })   
         }     
     }
     
     componentWillUnmount() {
-        loaded = this.state.teams;
+        loaded = this.state.matches;
     }
 
     render() {
@@ -45,9 +45,7 @@ export class Matches extends React.Component {
             }
             const colFormatter = (cell, row) => {
                 return (
-                    <Link to={'/matches/' + row.idMatch}>
-                        {cell}
-                    </Link>
+                    <Link to={'/matches/' + row.idMatch}> { cell } </Link>
                 )
             }
             return (
@@ -61,8 +59,33 @@ export class Matches extends React.Component {
             )
         }
 
-        const match = () => {
-
+        const match = (props) => {
+            if (loading) {
+                return <h2>Loading...</h2>;
+            }
+            const colFormatter = (cell, row) => {
+                return (
+                    <Link to={'/championships/'}> { cell } </Link>
+                )
+            }
+            const colFormatter2 = (cell, row) => {
+                return (
+                    <div>
+                        <Link to={'/teams/' + row.idFirstTeam}> { row.firstTeam } </Link>
+                        {' ' + cell + ' '}
+                        <Link to={'/teams/' + row.idSecondTeam}> { row.secondTeam } </Link>
+                    </div>
+                )
+            }
+            return (
+                <div>
+                    <BootstrapTable data={ matches.filter(item => item.idMatch === props.match.params.idMatch) } className='table-match'>
+                        <TableHeaderColumn dataField='date' width='17%'>Date</TableHeaderColumn>
+                        <TableHeaderColumn dataField='title' dataFormat={ colFormatter } width='36%'>Championship</TableHeaderColumn>
+                        <TableHeaderColumn dataField='score' dataFormat={ colFormatter2 } isKey={ true }>Match</TableHeaderColumn>
+                    </BootstrapTable>
+                </div>
+            )
         }
 
         return (
@@ -73,75 +96,3 @@ export class Matches extends React.Component {
         )
      }
 }
-
-/*
-
-
-export class Teams extends React.Component {
-
-    render() {
-         
-        const team = (props) => {
-            const tea = (item) => item.id_teams === props.match.params.id_teams;
-            const index = teams.findIndex(tea);
-            if (loading) {
-                return <h2>Loading...</h2>;
-            }
-            const SecondName = () => {        
-                return teams[index].second_name ? <p><strong>Друга назва: </strong>{teams[index].second_name}</p> : ''
-            }
-            const City = () => {        
-                return teams[index].city ? <p><strong>Місто: </strong>{teams[index].city}</p> : ''
-            }
-            const FoundationYear = () => {        
-                return teams[index].foundation_year > 0 ? <p><strong>Рік заснування: </strong>{teams[index].foundation_year}</p> : ''
-            }
-            const Coach = () => {        
-                return teams[index].coach ? <p><strong>Тренер: </strong>{teams[index].coach}</p> : ''
-            }
-            const President = () => {        
-                return teams[index].president ? <p><strong>Президент: </strong>{teams[index].president}</p> : ''
-            }
-            const HomeStadion = () => {        
-                return teams[index].home_stadion ? <p><strong>Домашній стадіон: </strong>{teams[index].home_stadion}</p> : ''
-            }
-            const IdChampionship = () => {        
-                return teams[index].id_championship ? 
-                <p><strong>ID чемпіонату: </strong><Link to={'/championships/' + teams[index].id_championship}>{teams[index].id_championship}</Link></p> : ''
-            }
-            const OffSite = () => {        
-                return teams[index].off_site ? <p><strong>Веб сторінка: </strong><a href={teams[index].off_site}>{teams[index].off_site}</a></p> : ''
-            }
-            function createMarkup() {
-                return {__html: teams[index].title};
-              }
-            const Title = () => {        
-                return teams[index].title.length < 45  ? '' : <div><p><strong>Трофеї: </strong></p><div dangerouslySetInnerHTML={createMarkup()} /></div>
-            }
-            return (
-                <div className="container left">
-                    <img src={teams[index].emblema} />
-                    <p><strong>Назва команди: </strong>{teams[index].name}</p>
-                    <SecondName />
-                    <City />
-                    <FoundationYear />
-                    <Coach />
-                    <President />
-                    <HomeStadion />
-                    <IdChampionship />
-                    <OffSite />
-                    <Title />
-                </div>              
-            )
-        }
-        
-        return (
-            <Switch>
-                <Route exact path='/teams' component={teamsAll}/>
-                <Route exact path='/teams/:id_teams' component={team}/>
-            </Switch>
-        )
-     }
-}
-
-*/
